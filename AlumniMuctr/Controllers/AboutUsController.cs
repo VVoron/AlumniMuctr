a4eacf57f0e84fe535bcc5ed35edbc2e9aab1f72
VVoron <vkunievskiy@list.ru>
2023-01-29 14:46:48 +0300

    Fixed about us and logo

--- AlumniMuctr/Controllers/AboutUsController.cs
+++ AlumniMuctr/Controllers/AboutUsController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlumniMuctr.Data;
+using AlumniMuctr.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlumniMuctr.Controllers
 {
     public class AboutUsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public AboutUsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            Helper obj = new Helper();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HelperRequest(Helper obj)
+        {
+            _db.Helper.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
